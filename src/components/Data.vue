81cec6cc1cc9d9b3b137dde903acbead7d43edb4
ishanbh <theishanbh@gmail.com>
2022-02-19 19:41:22 +0530

    commit

---
+++ src/components/Data.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Options - {{o_firstname}} </h2>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Data',
+  setup(){
+    const c_firstname = ref('Clark')
+    return{
+      c_firstname
+    }
+  },
+  data() {
+    return{
+      o_firstname : 'Bruce',
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
